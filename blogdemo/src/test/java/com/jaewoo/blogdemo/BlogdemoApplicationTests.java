376b8e5bf42d16c1b15ac2415a0828f6a0a26951
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-01-20 17:14:44 +0900

    project init - blogdemo

---
+++ blogdemo/src/test/java/com/jaewoo/blogdemo/BlogdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewoo.blogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
