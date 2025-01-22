1863ae6f74aa21c060fc1aa009fb47abcd57dbe2
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-01-22 14:47:20 +0900

    test : user,article,comment 테스트 코드 작성

---
+++ blogdemo/src/test/java/com/jaewoo/blogdemo/article/db/ArticleRepositoryTest.java
@@ -0,0 +1,115 @@
+package com.jaewoo.blogdemo.article.db;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.jaewoo.blogdemo.article.entity.Article;
+import com.jaewoo.blogdemo.common.config.JpaAuditingConfiguration;
+import com.jaewoo.blogdemo.user.db.UserRepository;
+import com.jaewoo.blogdemo.user.entity.Email;
+import com.jaewoo.blogdemo.user.entity.User;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@Import(JpaAuditingConfiguration.class)
+@TestPropertySource(locations = {"classpath*:application.yaml"})
+class ArticleRepositoryTest {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    private Article article;
+
+    @BeforeEach
+    void setUp() {
+        user = User.create("김길동", Email.of("kim@email.com"), "123123");
+        userRepository.save(user);
+        article = Article.create("test title", "test content", user);
+    }
+
+    @Test
+    void 글을_생성한다() {
+        // given
+        User user = userRepository.save(
+                User.create(
+                        "홍길동",
+                        Email.of("hong@mail.com"),
+                        "123123")
+        );
+        String title = "test1";
+        String content = "이 글이 제대로 테스트 동작하는지 확인합니다.";
+
+        // when
+        var createdArticle = Article.create(title, content, user);
+        var savedArticle = articleRepository.save(createdArticle);
+
+        // then
+        Assertions.assertAll(
+                () -> {
+                    assertThat(savedArticle.getTitle()).isEqualTo(title);
+                    assertThat(savedArticle.getContent()).isEqualTo(content);
+                    assertThat(savedArticle.getUser()).isEqualTo(user);
+                }
+        );
+    }
+
+    @Test
+    void 글을_저장한_후_모든_글_조회() {
+        //given
+        articleRepository.save(article);
+        // when
+        List<Article> articles = articleRepository.findAll();
+        // then
+        assertThat(articles).isNotEmpty();
+        assertThat(articles.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 글의_제목을_수정하고_조회_변경된_제목으로_반환() {
+        //given
+        var savedArticle = articleRepository.save(article);
+        String changeTitle = "change test title";
+        // when
+        savedArticle.changeTitle(changeTitle);
+        var updatedArticle = articleRepository.save(savedArticle);
+        // then
+        assertThat(updatedArticle.getTitle()).isEqualTo(changeTitle);
+    }
+
+    @Test
+    void 글의_내용을_수정하고_업데이트된_글_조회_반환() {
+        //given
+        var savedArticle = articleRepository.save(article);
+        String changeContent = "change test content";
+        // when
+        savedArticle.changeContent(changeContent);
+        var updatedArticle = articleRepository.save(savedArticle);
+        // then
+        assertThat(updatedArticle.getContent()).isEqualTo(changeContent);
+    }
+
+    @Test
+    void 글을_삭제후_조회_시_비어있는_Optional반환() {
+        //given
+        articleRepository.save(article);
+        // when
+        articleRepository.deleteById(article.getId());
+        Optional<Article> articleOptional = articleRepository.findById(article.getId());
+        // then
+        assertThat(articleOptional).isEmpty();
+    }
+}
---
+++ blogdemo/src/test/java/com/jaewoo/blogdemo/comment/db/CommentRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.jaewoo.blogdemo.comment.db;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.jaewoo.blogdemo.article.db.ArticleRepository;
+import com.jaewoo.blogdemo.article.entity.Article;
+import com.jaewoo.blogdemo.comment.entity.Comment;
+import com.jaewoo.blogdemo.common.config.JpaAuditingConfiguration;
+import com.jaewoo.blogdemo.user.db.UserRepository;
+import com.jaewoo.blogdemo.user.entity.Email;
+import com.jaewoo.blogdemo.user.entity.User;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@Import(value= {JpaAuditingConfiguration.class})
+class CommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    private User user = User.create("홍길동", Email.of("hong@mail.com"), "123123");
+    private Article article = Article.create("test subject", "test content abcedf", user);
+    private Comment comment;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(user);
+        articleRepository.save(article);
+        comment = Comment.create("댓글 테스트", user, article);
+    }
+
+    @Test
+    void 댓글생성_저장() {
+        // given
+        // when
+        var savedComment = articleRepository.save(article);
+        // then
+        assertThat(savedComment).isNotNull();
+        assertThat(savedComment.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void 새로운_댓글_저장후_모든_댓글_조회() {
+        // given
+        articleRepository.save(article);
+        // when
+        List<Article> articles = articleRepository.findAll();
+        // then
+        assertThat(articles).isNotEmpty();
+        assertThat(articles.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 저장되어있는_댓글_id_조회() {
+        //given
+        commentRepository.save(comment);
+        // when
+        Optional<Comment> foundComment = commentRepository.findById(comment.getId());
+        // then
+        assertThat(foundComment).isNotEmpty();
+    }
+
+    @Test
+    void 댓글_수정_정상적으로_수정됨() {
+        //given
+        var savedComment = commentRepository.save(comment);
+        String changeContent = "change test";
+        // when
+        savedComment.changeContent(changeContent);
+        Comment updatedComment = commentRepository.save(savedComment);
+        // then
+        assertThat(updatedComment.getContent()).isEqualTo(changeContent);
+    }
+
+    @Test
+    void 댓글_삭제_조회시_빈Optional반환() {
+        //given
+        commentRepository.save(comment);
+        // when
+        commentRepository.deleteById(comment.getId());
+        Optional<Comment> deletedComment = commentRepository.findById(comment.getId());
+        // then
+        assertThat(deletedComment).isEmpty();
+    }
+}
